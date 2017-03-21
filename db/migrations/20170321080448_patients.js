76d84a34cd57260a3676edd93dfcb70c43cd2af4
Gabi Procell <gprocell927@gmail.com>
2017-03-21 13:42:46 -0600

    creates tables and knexfile

---
+++ db/migrations/20170321080448_patients.js
@@ -0,0 +1,53 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('patients', function(table) {
+      table.increments('id').primary()
+      table.string('name')
+      table.string('sex')
+      table.string('species')
+      table.date('dob')
+      table.timestamps
+    }),
+
+    knex.schema.createTable('procedures', function(table) {
+      table.increments('id').primary()
+      table.date('date')
+      table.string('surgeon')
+      table.string('anesthetist')
+      table.integer('patient_id')
+            .references('id')
+            .inTable('patients')
+      table.json('readings')
+      table.time('start_time')
+      table.time('end_time')
+      table.text('notes')
+    }),
+
+    knex.schema.createTable('readings', function(table) {
+      table.timestamp
+      table.decimal('temperature')
+      table.integer('pulse')
+      table.integer('respirations')
+      table.decimal('oxygen')
+      table.decimal('vaporizer')
+      table.string('gas_agent')
+      table.integer('systolic_bp')
+      table.integer('diastoli_bp')
+      table.integer('mean_bp')
+      table.integer('etco2')
+      table.integer('spo2')
+      table.integer('procedure_id')
+            .references('id')
+            .inTable('procedures')
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('patients'),
+    knex.schema.dropTable('procedures'),
+    knex.schema.dropTable('readings')
+  ])
+};
---
+++ knexfile.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection:'postgres://localhost/byob',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/byob_test',
+    migrations: {
+      directory: './db.migrations'
+    },
+    useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/producion'
+    },
+    useNullAsDefault: true
+  }
+}
