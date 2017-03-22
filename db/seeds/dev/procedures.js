328d7b54a056e8488af1061ec896f0f5fa32d0c6
Gabi Procell <gprocell927@gmail.com>
2017-03-21 18:41:34 -0600

    adds procedure seed data

--- db/seeds/dev/procedures.js
+++ db/seeds/dev/procedures.js
@@ -202,61 +202,104 @@ exports.seed = function(knex, Promise) {
         notes:"Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
       }),
       knex('procedures').insert({
-        
+        date:"4/25/2016",
+        surgeon:"Robert",
+        anesthetist:"Naëlle",
+        patient_id:21,
+        readings:[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        start_time:"2:08 AM",
+        end_time:"9:00 AM",
+        notes:"Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat."
       }),
       knex('procedures').insert({
-        date: 'Snake Pliskin',
-        surgeon:'M',
-        anesthetist:'Reptile',
-        patient_id: 07/22/2010
+        date:"1/13/2017",
+        surgeon:"Carl",
+        anesthetist:"Crééz",
+        patient_id:22,
+        readings:[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        start_time:"2:05 PM",
+        end_time:"8:21 PM",
+        notes:"Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum."
       }),
       knex('procedures').insert({
-        date: 'Velcro Jiminez',
-        surgeon:'F',
-        anesthetist:'Sloth',
-        patient_id: 01/01/2006
+        date:"8/24/2016",
+        surgeon:"Benjamin",
+        anesthetist:"Léonore",
+        patient_id:23,
+        readings:[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        start_time:"7:31 AM",
+        end_time:"8:32 AM",
+        notes:"Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque."
       }),
       knex('procedures').insert({
-        date: 'Monk Kraemer',
-        surgeon:'MN',
-        anesthetist:'Canine',
-        patient_id: 08/22/2007
+        date:"10/21/2016",
+        surgeon:"Henry",
+        anesthetist:"Régine",
+        patient_id:24,
+        readings:[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        start_time:"10:37 AM",
+        end_time:"7:11 AM",
+        notes:"In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo."
       }),
       knex('procedures').insert({
-        date: 'Bailey',
-        surgeon:'MN',
-        anesthetist:'Canine',
-        patient_id: 08/14/2002
+        date:"11/23/2016",
+        surgeon:"Harold",
+        anesthetist:"Maëlann",
+        patient_id:25,
+        readings:[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        start_time:"9:52 AM",
+        end_time:"6:17 AM",
+        notes:"In congue. Etiam justo. Etiam pretium iaculis justo."
       }),
       knex('procedures').insert({
-        date: 'Max',
-        surgeon:'SF',
-        anesthetist:'Lizard',
-        patient_id: 09/27/2003
+        date:"10/11/2016",
+        surgeon:"Janet",
+        anesthetist:"Andréa",
+        patient_id:26,
+        readings:[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        start_time:"9:19 AM",
+        end_time:"7:43 PM",
+        notes:"Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus."
       }),
       knex('procedures').insert({
-        date: 'Bella'
-        surgeon:'SF',
-        anesthetist:'Feline',
-        patient_id: 07/21/2006
+        date:"8/5/2016",
+        surgeon:"Amy",
+        anesthetist:"Dù",
+        patient_id:27,
+        readings:[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        start_time:"4:04 PM",
+        end_time:"8:27 AM",
+        notes:"Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh."
       }),
       knex('procedures').insert({
-        date: 'Barley',
-        surgeon:'M',
-        anesthetist:'Canine',
-        patient_id: 07/21/2011
+        date:"10/8/2016",
+        surgeon:"Ashley",
+        anesthetist:"Illustrée",
+        patient_id:28,
+        readings:[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        start_time:"2:40 AM",
+        end_time:"2:42 AM",
+        notes:"Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede."
       }),
       knex('procedures').insert({
-        date: 'Everett',
-        surgeon:'M',
-        anesthetist:'Canine',
-        patient_id: 03/21/2000
+        date:"12/14/2016",
+        surgeon:"Jonathan",
+        anesthetist:"Tán",
+        patient_id:29,
+        readings:[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        start_time:"5:11 AM",
+        end_time:"2:52 PM",
+        "notes":"Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros."
       }),
       knex('procedures').insert({
-        date: 'Fiona'
-        surgeon:'SF',
-        anesthetist:'Feline',
-        patient_id: 07/31/2010
+        date:"5/3/2016",
+        surgeon:"Ruby",
+        anesthetist:"Hélène",
+        patient_id:30,
+        readings:[{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}],
+        start_time:"2:00 AM",
+        end_time:"1:44 PM",
+        notes:"Sed ante. Vivamus tortor. Duis mattis egestas metus."
       })
     ]);
   });
