648a809ff10ec4edd3940e3944d68fe4bee55a1b
Gabi Procell <gprocell927@gmail.com>
2017-03-26 21:22:06 -0600

    adds procfile and configs production environment

--- knexfile.js
+++ knexfile.js
@@ -26,9 +26,9 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
-      directory: __dirname + '/db/migrations'
+      directory: './db/migrations'
     },
     seeds: {
       directory: './db/seeds/production'
